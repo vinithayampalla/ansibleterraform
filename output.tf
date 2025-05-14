c73e956a518657403da8489098c21103098397ba
vinithayampalla <yampallavini@gamil.com>
2025-05-14 18:02:22 +0530

    anitha

--- output.tf
+++ output.tf
@@ -1,7 +1,7 @@
-output "name" {
-    value = aws_instance.ansible_terraform_jenkinsinstance.id
+# output "name" {
+#     value = aws_instance.ansible_terraform_jenkinsinstance.id
   
-}
+# }
 output "sgid"{
     value = aws_security_group.ec2_sg_ssh_http.id
 } 
